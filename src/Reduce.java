5648031095939e3ee0dcb9696cea405c96f4c647
SPHA <phiaxy28@gmail.com>
2025-05-08 19:07:59 -0400

    new class

---
+++ src/Reduce.java
@@ -0,0 +1,17 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int num = 100;
+        int steps = 0;
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num = num / 2;
+                steps++;
+            }
+            else{
+                num = num - 1;
+                steps++;
+            }
+        }
+        System.out.println(steps);
+    }
+}
